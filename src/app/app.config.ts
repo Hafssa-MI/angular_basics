01f9be3c0ce04ca1d14c6ebfd3bde70fb258e444
HAFSSA <hafssa.miftah-etu@etu.univh2c.ma>
2026-04-13 18:17:00 +0100

    creation d'angular project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)]
+    // Si on veut communiquer avec le backend on ajoute le module http client pr exmpl
+    //provideHTTPClient() // si on travaille avec une version avec modules on utilisera app.module.ts dans imports []
+
 };
